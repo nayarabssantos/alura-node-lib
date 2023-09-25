84383c61cf2b30bc962957864a14c7e8f9361e91
Nayara Bruna Silva dos Santos <nayarabrunasilvadossantos@fico.com>
2023-09-25 14:47:16 -0300

    lendo arquivo e tratando erro

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('olá mundo');
+import fs from 'fs';
+import chalk from 'chalk';
+
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+function pegaArquivo(caminhoDoArquivo){
+    const encoding = 'UTF-8';
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+        if (erro){
+            trataErro(erro);
+        }
+        console.log(chalk.green(texto));
+    })
+}
+
+pegaArquivo('./arquivos/texto.md');
